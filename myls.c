7ff27c2ca2d50e1312bc534cb58be961f3674e13
NaimDrive <naimdrive98@gmail.com>
2020-10-25 12:02:26 +0100

    :sparkles: includes in myls

--- myls.c
+++ myls.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+#define RED(m) "\033[01;31m" m "\033[0m"
+#define GREEN(m) "\033[01;32m" m "\033[0m"
 
 int main(int argv, char *argc[])
 {
