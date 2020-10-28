8653179f7cc16494fe6470e3ff59a684280fd551
NaimDrive <naimdrive98@gmail.com>
2020-10-28 12:44:53 +0100

    :sparkles: mysh.h added

---
+++ mysh.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+#define ERR -1
+#define FAILED_EXEC 127
+
+/**
+ * Réécriture de la fonction system() vu en cours.
+ * Version moins lourde, basé sur la fonction execl. 
+ * @param command
+ * */
+int systemV2(char *command);
+
+/**
+ * Fonction vidant le buffer
+ * @param buffer
+ * */
+void clearBuffer(char *buffer);
