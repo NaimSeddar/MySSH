245cd2ffee01ce78e1219d98967423502c7f742d
NaimDrive <naimdrive98@gmail.com>
2020-11-10 20:20:04 +0100

    :sparkles: add utils

---
+++ utils.c
@@ -0,0 +1,52 @@
+#include "utils.h"
+
+/*
+ * Better strtok
+ */
+char **str_split(char *str, const char delimiter)
+{
+    char **result = 0;
+    size_t count = 0;
+    char *tmp = str;
+    char *last_comma = 0;
+    char delim[2];
+    delim[0] = delimiter;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (delimiter == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (str + strlen(str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char *) * count);
+
+    if (result)
+    {
+        size_t idx = 0;
+        char *token = strtok(str, delim);
+
+        while (token)
+        {
+            if (idx >= count)
+                exit(-1);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
---
+++ utils.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Découpe un string par un délimiteur donné, et retourne un tableau de string. 
+ */
+char **str_split(char *str, const char delimiter);
