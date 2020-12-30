8131a01b2917f18514c990fd88d4b13c30e37b65
NaimSeddar <naimdrive98@gmail.com>
2020-12-30 12:02:45 +0100

    :sparkles: colors .c .h clean

--- includes/colors.h
+++ includes/colors.h
@@ -1,7 +1,7 @@
 /**
  * Auteur:                Seddar Naïm
  * Création:              25/10/2020 12:27:36
- * Dernière modification: 20/12/2020 12:22:32
+ * Dernière modification: 30/12/2020 12:01:55
  * Master 1 Informatique
  */
 
@@ -20,9 +20,22 @@
 #define WHITE_C "\033[1;37m"
 #define RESET_C "\033[0m"
 
+// Underline
 #define U_RED_C "\033[4;31m"
 #define U_WHITE_C "\033[4;37m"
 
+/**
+ * Put a gray background in the terminal.
+ */
 void grayBG();
+
+/**
+ * Put a white background in the terminal.
+ */
 void whiteBG();
+
+/**
+ * Write in the given color
+ * @param  color  The color we want to use.
+ */
 void writein(const char *color);
